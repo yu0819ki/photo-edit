8964139eaeaa56f59ee1e0def536c396aa9480dc
yu0819ki <yu0819ki@gmail.com>
2019-10-15 23:46:34 +0900

    VueでHelloWorld

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-require('./bootstrap');
+import Vue from 'Vue';
+import Hello from '../vue/apps/Hello';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(Hello),
+}).$mount('#app');
