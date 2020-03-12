34fc38b67bc9d90aeb4a620509091316f0670e09
yu0819ki <yu0819ki@gmail.com>
2020-03-12 23:48:45 +0900

    ready to use custom filter

--- resources/vue/components/Upload.vue
+++ resources/vue/components/Upload.vue
@@ -12,6 +12,9 @@
         <b-field label="Brightness">
           <b-slider v-model="brightness" :min="0" :max="200" :custom-formatter="val => (val - 100) + '%'" lazy @change="applyFilter" ></b-slider>
         </b-field>
+        <b-field label="Temperature">
+          <b-slider v-model="temperature" :min="0" :max="200" :custom-formatter="val => (val - 100) + '%'" lazy @change="applyTempFilter" ></b-slider>
+        </b-field>
       </div>
       <vue-croppa
         class="canvas"
@@ -24,8 +27,11 @@
         remove-button-color="#666"
         :prevent-white-space="true"
         :quality="4"
+        :initial-image="initialImage"
         @init="init"
         @draw="init"
+        @new-image-drawn="handleNewImageDrawn"
+        @image-remove="handleRemove"
       />
     </div>
   </div>
@@ -47,6 +53,9 @@ export default {
       useAutoSizing: false,
       contrast: 100,
       brightness: 100,
+      temperature: 100,
+      originalImage: null,
+      initialImage: null,
     };
   },
   computed: {
@@ -67,7 +76,39 @@ export default {
       const ctx = this.image.ctx;
       ctx.filter = `contrast(${this.contrast}%) brightness(${this.brightness}%)`;
       this.image._draw();
-    }
+    },
+    applyTempFilter() {
+      const originalImage = this.originalImage;
+      const newImage = new Image();
+      const newCanvas = document.createElement('canvas');
+      newImage.onload = () => {
+        newCanvas.width = originalImage.naturalWidth;
+        newCanvas.height = originalImage.naturalHeight;
+        const ctx = newCanvas.getContext('2d');
+        ctx.drawImage(newImage, 0, 0);
+        const imageData = ctx.getImageData(0, 0, originalImage.naturalWidth, originalImage.naturalHeight);
+        const data = imageData.data;
+        for (let i = 0; i < data.length; i += 4) {
+          // 255-(r|g|b)
+          data[i]   = 255 - data[i]  ;
+          data[i+1] = 255 - data[i+1];
+          data[i+2] = 255 - data[i+2];
+        }
+        ctx.putImageData(imageData, 0, 0);
+
+        this.initialImage = newCanvas.toDataURL('image/png');
+        this.image.refresh();
+      };
+      newImage.src = originalImage.src;
+    },
+    handleNewImageDrawn() {
+      if (!this.originalImage) {
+        this.originalImage = this.image.originalImage;
+      }
+    },
+    handleRemove() {
+      this.originalImage = null;
+    },
   },
 }
 </script>
